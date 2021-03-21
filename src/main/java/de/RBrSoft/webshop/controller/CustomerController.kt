be8fa482d24fb23b8a02c85523b6fab8d3f64b3f
Roman Breuer <roman.breuer@rbrsoft.de>
2021-03-21 17:30:48 +0100

    Customers

---
+++ src/main/java/de/RBrSoft/webshop/controller/CustomerController.kt
@@ -0,0 +1,27 @@
+package de.RBrSoft.webshop.controller
+
+import de.RBrSoft.webshop.model.CustomerResponse
+import de.RBrSoft.webshop.repository.CustomerRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CustomerController {
+
+    val customerRepository: CustomerRepository = CustomerRepository()
+
+    @GetMapping("/customers/{id}")
+    fun getCustomerById(
+            @PathVariable id: String): ResponseEntity<CustomerResponse> {
+
+        val customer = customerRepository.findById(id)
+        return if (customer != null)
+            ResponseEntity.ok(customer)
+        else
+            ResponseEntity.notFound().build()
+    }
+
+
+}
---
+++ src/main/java/de/RBrSoft/webshop/repository/CustomerRepository.kt
@@ -0,0 +1,33 @@
+package de.RBrSoft.webshop.repository
+
+import de.RBrSoft.webshop.model.CustomerResponse
+import java.util.*
+
+class CustomerRepository {
+
+    val customers = listOf(
+            CustomerResponse(
+                    "1", // UUID.randomUUID().toString(),
+                    "Roman",
+                    "Breuer",
+                    "roman.breuer@rbrSoft.de"
+            ),
+            CustomerResponse(
+                    UUID.randomUUID().toString(),
+                    "Anne",
+                    "Grobleben",
+                    "anne.grobleben@rbrSoft.de"
+            ),
+            CustomerResponse(
+                    UUID.randomUUID().toString(),
+                    "Clint",
+                    "Eastwood",
+                    "weiss@ich.net"
+            )
+    )
+
+    fun findById(id: String): CustomerResponse? {
+        return customers.find { it.id == id }
+    }
+
+}
